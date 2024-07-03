96263a641e79189fa32e5a1f1ca3bf2ba32e0f0a
tintanaa <miiagkov123456@gmail.com>
2024-07-03 19:15:43 +0600

    feat: category widget

---
+++ lib/src/features/menu/view/category_grid.dart
@@ -0,0 +1,24 @@
+import 'package:coffee_shop/src/features/menu/data/category_data.dart';
+import 'package:coffee_shop/src/features/menu/view/product_card.dart';
+import 'package:flutter/material.dart';
+
+class CategoryGridView extends StatelessWidget {
+  final CategoryData category;
+
+  const CategoryGridView({Key? key, required this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      childAspectRatio: 0.7,
+      crossAxisSpacing: 8,
+      mainAxisSpacing: 8,
+      children: category.products.map((product) {
+        return ProductCard(product: product);
+      }).toList(),
+    );
+  }
+}
