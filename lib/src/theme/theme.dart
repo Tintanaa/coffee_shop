aa162d5275d89b78dfbd12fab4b34ca86d611857
tintanaa <miiagkov123456@gmail.com>
2024-07-03 17:44:26 +0600

    feat: colors, themes, placeholders

--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -1,6 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:coffee_shop/src/theme/app_colors.dart';
 
 final theme = ThemeData(
   cardTheme: const CardTheme(),
-  textTheme: const TextTheme(),
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+      fontFamily: 'Roboto', 
+      fontSize: 32, 
+      fontWeight: FontWeight.w600, 
+      letterSpacing: 0, 
+      color: AppColors.black,),
+
+    titleMedium: TextStyle(
+      fontFamily: 'Roboto', 
+      fontSize: 16, 
+      fontWeight: FontWeight.w500, 
+      letterSpacing: 0.15, 
+      color: AppColors.black,),
+
+    bodyMedium: TextStyle(
+      fontFamily: 'Roboto', 
+      fontSize: 14, 
+      fontWeight: FontWeight.w400, 
+      letterSpacing: 0.25, 
+      color: AppColors.black,),
+
+    bodySmall: TextStyle(
+      fontFamily: 'Roboto', 
+      fontSize: 12, 
+      fontWeight: FontWeight.w400, 
+      letterSpacing: 0.4, 
+      color: AppColors.white,),
+  ),
 );
