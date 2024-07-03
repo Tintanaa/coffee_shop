6ecc56984cbf83db14292b1bcdf034a0dedae7d4
tintanaa <miiagkov123456@gmail.com>
2024-07-03 17:25:42 +0600

    feat: mocks data

---
+++ lib/src/features/menu/models/product_mocks.dart
@@ -0,0 +1,74 @@
+import 'package:coffee_shop/src/features/menu/data/product_data.dart';
+
+ProductData product = const ProductData(
+  name: 'Эспрессо', 
+  price: 67.99, 
+  currency: 'руб', 
+  imageurl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzAWmqzbm8rAIbeoc4egRX9tjkp6HZoxWuzQ&s',);
+
+ProductData product1 = const ProductData(
+  name: 'Раф', 
+  price: 209.99, 
+  currency: 'руб', 
+  imageurl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Rafcoffe.jpg/274px-Rafcoffe.jpg',);
+
+ProductData product2 = const ProductData(
+  name: 'Мокачино',
+  price: 3,
+  currency: 'euro',
+  imageurl: 'https://coffeemachina.com/wp-content/uploads/2019/06/mokachino.jpg',);
+
+ProductData product3 = const ProductData(
+  name: 'Латте', 
+  price: 230.99, 
+  currency: 'руб', 
+  imageurl: 'https://coffeepedia.ru/wp-content/uploads/2013/01/coffee.jpg',);
+
+ProductData product4 = const ProductData(
+  name: 'Американо', 
+  price: 99, 
+  currency: 'руб', 
+  imageurl: 'https://coffe-spb.ru/img/cms/americano.jpg',);
+
+ProductData product5 = const ProductData(
+  name: 'Олеато', 
+  price: 160, 
+  currency: 'руб', 
+  imageurl: 'https://cdn.bg.ru/bg.ru/post_image-image/peiaRAV-RUEM26X8-8v5zw.png',);
+
+ProductData product6 = const ProductData(
+  name: 'Лунго',
+  price: 90,
+  currency: 'руб',
+  imageurl: 'https://nescafe.ru/sites/default/files/styles/topic_intro_large_webp/public/lungo.jpg.webp?itok=6jduJ4zC',);
+
+ProductData product7 = const ProductData(
+  name: 'Ристретто',
+  price: 110,
+  currency: 'руб',
+  imageurl: "https://nescafe.ru/sites/default/files/styles/topic_intro_large_webp/public/ristretto.jpg.webp?itok=3mg3AQQh",);
+
+ProductData product8 = const ProductData(
+  name: 'Флэт уайт',
+  price: 120,
+  currency: 'руб',
+  imageurl: 'https://nescafe.ru/sites/default/files/styles/topic_intro_large_webp/public/flat-white.jpg.webp?itok=GoMlYmh4',);
+
+ProductData product9 = const ProductData(
+  name: 'Аффогато',
+  price: 150,
+  currency: 'руб',
+  imageurl: 'https://storage.yandexcloud.net/moskvichmag/uploads/2022/07/shutterstock_417033919.jpg',);
+
+ProductData product10 = const ProductData(
+  name: 'Капуччино',
+  price: 140,
+  currency: 'руб',
+  imageurl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzazKBBL7OzCXJgZ2h0dAnRjo2VUl8tVHM-A&s',);
+
+ProductData product11 = const ProductData(
+  name: 'Dunkaccino',
+  price: 5,
+  currency: 'usd',
+  imageurl: 'https://nypost.com/wp-content/uploads/sites/2/2023/03/NYPICHPDPICT000008362441.jpg?quality=75&strip=all&w=1024',);
+
